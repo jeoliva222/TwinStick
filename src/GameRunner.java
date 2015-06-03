625b4c95fb877316c8756ce632ac45a3556f31d8
jeoliva222 <jeoliva@wpi.edu>
2015-06-03 15:06:13 -0400

    Menus now work

---
+++ src/GameRunner.java
@@ -0,0 +1,11 @@
+
+import game.views.MainMenu;
+
+public class GameRunner {
+
+	public static void main(String[] args) {
+		MainMenu mainScreen = new MainMenu();
+		mainScreen.setVisible(true);
+	}
+	
+}
