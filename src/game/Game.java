0e1c8f4d91eb1c60a3f6a68bd4ae2a828d34ecae
ModernShoe <themodernshoe@gmail.com>
2015-06-04 14:41:22 -0400

    Organization

--- src/game/Game.java
+++ src/game/Game.java
@@ -19,24 +19,6 @@ public class Game {
 	public void render() {
 		
 	}
-	
-	/**
-	 * @param x -- center of sprite x coordinate
-	 * @param y -- center of sprite y coordinate
-	 * @param sx -- size of sprite x
-	 * @param sy -- size of sprite y
-	 */
-	public void drawBox(float x, float y, float sx, float sy) {
-		glColor3f(1f, 1f, 1f);
-		glBegin(GL_QUADS);
-		{
-            glTexCoord2f(0, 0); glVertex2f(x - (sx/2), y - (sy/2));
-            glTexCoord2f(0, 1); glVertex2f(x - (sx/2), y + (sy/2));
-            glTexCoord2f(1, 1); glVertex2f(x + (sx/2), y + (sy/2));
-            glTexCoord2f(1, 0); glVertex2f(x + (sx/2), y - (sy/2));
-		}
-		glEnd();
-	}
 
 	/**
 	 * @param x -- center of sprite x coordinate
@@ -50,6 +32,24 @@ public class Game {
 	 */
 	public void drawBox(float x, float y, float sx, float sy, float r, float g, float b) {
 		glColor3f(r, g, b);
+		glBegin(GL_QUADS);
+		{
+            glTexCoord2f(0, 0); glVertex2f(x - (sx/2), y - (sy/2));
+            glTexCoord2f(0, 1); glVertex2f(x - (sx/2), y + (sy/2));
+            glTexCoord2f(1, 1); glVertex2f(x + (sx/2), y + (sy/2));
+            glTexCoord2f(1, 0); glVertex2f(x + (sx/2), y - (sy/2));
+		}
+		glEnd();
+	}
+	
+	/**
+	 * @param x -- center of sprite x coordinate
+	 * @param y -- center of sprite y coordinate
+	 * @param sx -- size of sprite x
+	 * @param sy -- size of sprite y
+	 */
+	public void drawBox(float x, float y, float sx, float sy) {
+		glColor3f(1f, 1f, 1f);
 		glBegin(GL_QUADS);
 		{
             glTexCoord2f(0, 0); glVertex2f(x - (sx/2), y - (sy/2));
